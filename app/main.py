f794b0d3947f7b14f5dee47cb14d3759c72e0299
heydhruv <heydhruvdave@gmail.com>
2024-12-26 19:24:18 +0530

    nested models

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+
+
+app = FastAPI()
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float
+    image: Image | None = None
+
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item": item}
+
+# can use list, set, dict over any model or req parameter
