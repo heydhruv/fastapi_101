80339295d2e25ee670b27ef98f027125ba7b0b05
heydhruv <heydhruvdave@gmail.com>
2025-01-27 17:06:31 +0530

    form models

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class FormData(BaseModel):
+    username: str
+    password: str
+
+
+@app.post("/login/")
+async def login(form_data: Annotated[FormData, Form()]):
+    return form_data
