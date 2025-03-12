4756c83f5ec716b56e0b44f80c06e35179e31f32
heydhruv <heydhruvdave@gmail.com>
2025-03-12 12:22:16 +0530

    middleware

---
+++ app/main.py
@@ -0,0 +1,16 @@
+import time
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = f"{process_time:.2f} seconds"
+    return response
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
